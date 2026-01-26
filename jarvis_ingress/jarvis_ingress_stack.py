e6f0a233027c640834deccfc6a9317d44ad5d883
Shashank Melkote <shashank.mr.1@gmail.com>
2026-01-26 18:55:34 -0500

    Add CDK assertions tests

---
+++ jarvis_ingress/jarvis_ingress_stack.py
@@ -0,0 +1,77 @@
+from aws_cdk import (
+    CfnOutput,
+    Duration,
+    Stack,
+    aws_apigateway as apigateway,
+    aws_lambda as _lambda,
+    aws_secretsmanager as secretsmanager,
+)
+from constructs import Construct
+
+
+class JarvisIngressStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        shared_secret = secretsmanager.Secret(
+            self,
+            "JarvisWebhookSharedSecret",
+            secret_name="jarvis/webhook/shared_secret",
+        )
+
+        router_fn = _lambda.Function(
+            self,
+            "RouterFunction",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            handler="app.handler",
+            code=_lambda.Code.from_asset("lambda/router"),
+        )
+
+        authorizer_fn = _lambda.Function(
+            self,
+            "AuthorizerFunction",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            handler="app.handler",
+            code=_lambda.Code.from_asset("lambda/authorizer"),
+            environment={
+                "SECRET_NAME": shared_secret.secret_name,
+                "MAX_SKEW_SECONDS": "300",
+            },
+        )
+        shared_secret.grant_read(authorizer_fn)
+
+        api = apigateway.RestApi(
+            self,
+            "JarvisIngressApi",
+            deploy_options=apigateway.StageOptions(stage_name="dev"),
+        )
+
+        authorizer = apigateway.RequestAuthorizer(
+            self,
+            "JarvisRequestAuthorizer",
+            handler=authorizer_fn,
+            identity_sources=[
+                apigateway.IdentitySource.header("x-jarvis-timestamp"),
+                apigateway.IdentitySource.header("x-jarvis-signature"),
+            ],
+            results_cache_ttl=Duration.seconds(0),
+        )
+
+        ingress = api.root.add_resource("ingress")
+        ingress.add_method(
+            "POST",
+            apigateway.LambdaIntegration(router_fn, proxy=True),
+            authorization_type=apigateway.AuthorizationType.CUSTOM,
+            authorizer=authorizer,
+        )
+
+        CfnOutput(
+            self,
+            "IngressUrl",
+            value=f"{api.url}ingress",
+        )
+        CfnOutput(
+            self,
+            "WebhookSecretArn",
+            value=shared_secret.secret_arn,
+        )
---
+++ tests/test_stack.py
@@ -0,0 +1,61 @@
+import aws_cdk as cdk
+from aws_cdk.assertions import Match, Template
+
+from jarvis_ingress.jarvis_ingress_stack import JarvisIngressStack
+
+
+def test_stack_resources():
+    app = cdk.App()
+    stack = JarvisIngressStack(
+        app,
+        "JarvisIngressStack",
+        env=cdk.Environment(region="us-east-1"),
+    )
+    template = Template.from_stack(stack)
+
+    template.resource_count_is("AWS::Lambda::Function", 2)
+    template.resource_count_is("AWS::ApiGateway::RestApi", 1)
+    template.resource_count_is("AWS::ApiGateway::Authorizer", 1)
+    template.resource_count_is("AWS::SecretsManager::Secret", 1)
+
+    template.has_resource_properties(
+        "AWS::Lambda::Function",
+        {
+            "Runtime": "python3.11",
+        },
+    )
+
+    template.has_resource_properties(
+        "AWS::SecretsManager::Secret",
+        {
+            "Name": "jarvis/webhook/shared_secret",
+        },
+    )
+
+    template.has_resource_properties(
+        "AWS::ApiGateway::Stage",
+        {
+            "StageName": "dev",
+        },
+    )
+
+    template.has_resource_properties(
+        "AWS::ApiGateway::Method",
+        {
+            "AuthorizationType": "CUSTOM",
+            "HttpMethod": "POST",
+        },
+    )
+
+    template.has_output(
+        "IngressUrl",
+        {
+            "Value": Match.any_value(),
+        },
+    )
+    template.has_output(
+        "WebhookSecretArn",
+        {
+            "Value": Match.any_value(),
+        },
+    )
